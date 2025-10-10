523725d1539dfc7034bb1a2d14c4b83824f9bce7
Neto <rlcneto12@gmail.com>
2025-10-09 22:00:09 -0300

    Level 2 Implementation

--- Code/EntityFactory.py
+++ Code/EntityFactory.py
@@ -19,6 +19,12 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))  # ESSA AQUI PASSA AS 7 PRIMEIRAS IMGAENS
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))   # ESSA PASSA MAIS 7 E O CICLO CONTINUA, PARA RODAR O JOGO NORMALMENTE
                 return list_bg
+            case 'Level2Bg':
+                list_bg = []
+                for i in range(5):
+                    list_bg.append(Background(f'Level2Bg{i}', (0, 0)))  # ESSA AQUI PASSA AS 7 PRIMEIRAS IMGAENS
+                    list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))  # ESSA PASSA MAIS 7 E O CICLO CONTINUA, PARA RODAR O JOGO NORMALMENTE
+                return list_bg
             case 'Player1':
                 return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
             case 'Player2':
